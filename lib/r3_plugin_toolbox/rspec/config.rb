293c7c247e8fb035bdad7a9976039faf8c7f3ab2
klacointe <kevinlacointe@gmail.com>
2011-01-31 11:04:44 +0100

    remove rspec requires

--- lib/r3_plugin_toolbox/rspec/config.rb
+++ lib/r3_plugin_toolbox/rspec/config.rb
@@ -1,14 +1,14 @@
-require 'rspec/core'
-require 'r3_plugin_toolbox/macro'
-require_all File.dirname(__FILE__) + '/matchers'
-
-RSpec.configure do |config|
-  config.include Rails3::Plugin::Extender::Matchers
-  config.extend Rails3::Plugin::Extender::Macro
-end
-
-module RSpec::Core
-  class ExampleGroup
-    include Rails3::Plugin::Extender::Macro
-  end
-end
+#require 'rspec/core'
+#require 'r3_plugin_toolbox/macro'
+#require_all File.dirname(__FILE__) + '/matchers'
+#
+#RSpec.configure do |config|
+#  config.include Rails3::Plugin::Extender::Matchers
+#  config.extend Rails3::Plugin::Extender::Macro
+#end
+#
+#module RSpec::Core
+#  class ExampleGroup
+#    include Rails3::Plugin::Extender::Macro
+#  end
+#end
